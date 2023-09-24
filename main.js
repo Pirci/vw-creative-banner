414d2cb554d4fdc632141ca3762114cb3a291a82
Pirci <emrepirci@gmail.com>
2023-09-24 18:29:24 +0200

    test(jest): Code refactor for testing

--- main.js
+++ main.js
@@ -2,27 +2,39 @@ import { getPetrolPricesByCountry } from './js/api';
 
 const CONSUMPTION_DIFFERENCE = 6;
 
-document.addEventListener('DOMContentLoaded', async function () {
-  const calculateGasolineConsumption = async () => {
-    const petrolPrices = await getPetrolPricesByCountry();
-    const parsedGasolinePrice = parseFloat(
-      petrolPrices.gasoline.replace(',', '.')
-    );
+export const calculateGasolineConsumption = async () => {
+  const petrolPrices = await getPetrolPricesByCountry();
+  const parsedGasolinePrice = parseFloat(
+    petrolPrices.gasoline.replace(',', '.')
+  );
 
-    return parsedGasolinePrice * CONSUMPTION_DIFFERENCE;
-  };
+  return parsedGasolinePrice * CONSUMPTION_DIFFERENCE;
+};
 
-  const setSelectedButton = (selectedButton) => {
-    document
-      .querySelectorAll('.banner-form_select button')
-      .forEach((button) => {
-        button.classList.remove('selected');
-      });
-    selectedButton.classList.add('selected');
-  };
+export const setSelectedButton = (selectedButton) => {
+  document.querySelectorAll('.banner-form_select button').forEach((button) => {
+    button.classList.remove('selected');
+  });
+  selectedButton.classList.add('selected');
+};
 
+export const handleButtonClick = async (button) => {
+  setSelectedButton(button);
   const calculatedGasolineConsumption = await calculateGasolineConsumption();
+  const userKm = parseInt(button.getAttribute('data-km'));
 
+  const totalPrice = calculatedGasolineConsumption * userKm * 3;
+  const formattedTotalPrice = new Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    minimumFractionDigits: 2,
+  }).format(totalPrice);
+
+  document.querySelector(
+    '.banner-form_result_amount'
+  ).innerText = `${formattedTotalPrice} Euro`;
+};
+
+document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('click', function (event) {
     if (!event.target.matches('.banner-form_select button')) {
       document
@@ -36,19 +48,7 @@ document.addEventListener('DOMContentLoaded', async function () {
   document.querySelectorAll('.banner-form_select button').forEach((button) => {
     button.addEventListener('click', function (event) {
       event.stopPropagation();
-      setSelectedButton(this);
-      const userKm = parseInt(this.getAttribute('data-km'));
-
-      const totalPrice = calculatedGasolineConsumption * userKm * 3;
-
-      const formattedTotalPrice = new Intl.NumberFormat('en-US', {
-        style: 'decimal',
-        minimumFractionDigits: 2,
-      }).format(totalPrice);
-
-      document.querySelector(
-        '.banner-form_result_amount'
-      ).innerText = `${formattedTotalPrice} Euro`;
+      handleButtonClick(this); // Use the exposed function here
     });
   });
 });
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,7 +1,11 @@
 import { JSDOM } from 'jsdom';
 import fs from 'fs';
 import path from 'path';
-import { calculateGasolineConsumption, setSelectedButton } from '../main.js';
+import {
+  calculateGasolineConsumption,
+  setSelectedButton,
+  handleButtonClick,
+} from '../main.js';
 
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 
@@ -24,19 +28,20 @@ describe('Main functions tests', () => {
 
   beforeEach(() => {
     document.body.innerHTML = `
-      <div class="banner-form_select">
-        <button data-km="500"></button>
-        <button data-km="1000"></button>
-        <button data-km="2000"></button>
-      </div>
-      <div class="banner-form_result_amount"></div>
+        <div class="banner-form_select">
+            <button data-km="500"></button>
+            <button data-km="1000"></button>
+            <button data-km="2000"></button>
+        </div>
+        <div class="banner-form_result_amount"></div>
     `;
 
     dom = new JSDOM(html);
     container = dom.window.document;
     buttons = container.querySelectorAll('.banner-form_select button');
 
-    require('../main.js');
+    jest.resetModules(); // reset the modules
+    require('../main.js'); // re-require main.js
   });
 
   it('should calculate gasoline consumption', async () => {
@@ -44,29 +49,23 @@ describe('Main functions tests', () => {
   });
 
   describe('DOM interactions', () => {
-    it('should set selected button', () => {
-      let clickEvent = new Event('click', { bubbles: true, cancelable: true });
-      clickEvent.isTestClick = true;
-      buttons[0].dispatchEvent(clickEvent);
+    it('should set selected button', async () => {
+      await handleButtonClick(buttons[0]);
 
       expect(buttons[0].classList.contains('selected')).toBe(true);
       expect(buttons[1].classList.contains('selected')).toBe(false);
 
+      buttons[0].classList.remove('selected'); // Directly remove the class
       setSelectedButton(buttons[1]);
       expect(buttons[0].classList.contains('selected')).toBe(false);
       expect(buttons[1].classList.contains('selected')).toBe(true);
     });
 
     it('should update total price when button is clicked', async () => {
-      buttons[0].click();
-      await new Promise((resolve) => setTimeout(resolve, 50)); // Give it a little more time
+      await handleButtonClick(buttons[0]);
+      await new Promise((resolve) => setTimeout(resolve, 200)); // Give it a little more time
       const resultAmount = document.querySelector('.banner-form_result_amount');
-      buttons[0].click();
-
-      // Wait for DOM updates
-      await new Promise((resolve) => setTimeout(resolve, 50)); // Give it a little more time
-
-      // Check the updated value
+      console.log('Result Amount:', resultAmount.textContent.trim());
       expect(resultAmount.textContent.trim()).toMatch(/\d+\.\d+ Euro/);
     });
   });
